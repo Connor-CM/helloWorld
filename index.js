bc2152ee0ba65485991b34547e134856384c1ae2
Connor-CM <connordechangebringer@computingforall.org>
2021-08-12 14:21:42 -0700

    Finished the project

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ const age = "18"
 firstName = "Other Connor"
 lastName = "C-M."
 
-console.log('Hello' + ' ' + firstName + ' ' + lastName + ' ' + 'Have a terrible day. Oh, and just in case you forgot, you are' + ' ' + age + '.');
+console.log('Hello' + ' ' + firstName + ' ' + lastName + ' ' + 'Have a completely average day. Oh, and just in case you forgot, you are' + ' ' + age + '.');
